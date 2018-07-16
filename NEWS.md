d4d21d99058feccf748d5eb98896b390615977de
obrl_soil <obrl-soil@users.noreply.github.com>
2018-07-16 20:35:37 +1000

    add h3_to_geo
    plus NEWS and README

---
+++ NEWS.md
@@ -0,0 +1,9 @@
+# v. 0.1.1
+ 
+  * `h3_to_geo()` added.
+  * 'geo_to_h3()` bugfix. A pox on devs who think y,x is ok.
+  * NEWS and README added.
+
+# v. 0.1.0
+
+  * `geo_to_h3()` - first function implemented.
--- tests/testthat/test_core_api.R
+++ tests/testthat/test_core_api.R
@@ -13,7 +13,9 @@ test_that('geo_to_h3 returns an appropriately structured data frame with single 
             expect_is(brissy$X, 'numeric'),
             expect_is(brissy$Y, 'numeric'),
             # res can be int or num
-            expect_is(brissy$h3_address, 'character')
+            expect_is(brissy$h3_address, 'character'),
+            # xy checks
+            expect_equal(brissy$h3_address, '8fbe8d12acad2f3')
           ))
 
 test_that('geo_to_h3 returns an appropriately structured data frame with multiple input',
@@ -27,4 +29,19 @@ test_that('geo_to_h3 returns an appropriately structured data frame with multipl
             expect_is(brissy$Y, 'numeric'),
             # res can be int or num
             expect_is(brissy$h3_address, 'character')
+          ))
+
+# h3_to_geo
+test_that('h3 to geo returns an appropriate dataset',
+          c(
+            brissy_10 <- h3_to_geo('8abe8d12acaffff'),
+            expect_is(brissy_10, 'data.frame'),
+            expect_equal(ncol(brissy_10), 3),
+            expect_equal(nrow(brissy_10), 1),
+            expect_equal(names(brissy_10), c('h3_address', 'h3_x', 'h3_y')),
+            expect_is(brissy_10$h3_x, 'numeric'),
+            expect_is(brissy_10$h3_y, 'numeric'),
+            # xy checks
+            expect_equal(brissy_10$h3_x, 153.0239032),
+            expect_equal(brissy_10$h3_y, -27.46852938)
           ))
