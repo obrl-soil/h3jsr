c26b56ec4a50ddda7d52d242c3f195776396f544
obrl_soil <obrl-soil@users.noreply.github.com>
2019-02-10 12:01:42 +1000

    update vignette, tidy various

--- R/h3jsr.R
+++ R/h3jsr.R
@@ -2,6 +2,5 @@
 #'
 #' This package uses [`V8`](https://github.com/jeroen/v8) to access the
 #' [javascript bindings for Uber's H3 library](https://github.com/uber/h3-js).
-#' It is intended as a temporary solution until direct R bindings are written.
 #'
 "_PACKAGE"
