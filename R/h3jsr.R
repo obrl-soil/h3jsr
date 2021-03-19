b0f06f81bbb3107d912275c9b824e4ce64e84f45
obrl-soil <obrl-soil@users.noreply.github.com>
2021-03-20 12:51:46 +1300

    fix manual landing page link formatting

--- R/h3jsr.R
+++ R/h3jsr.R
@@ -1,6 +1,6 @@
 #' h3jsr: Access Uber's H3 library
 #'
-#' This package uses [`V8`](https://github.com/jeroen/v8) to access the
-#' [javascript bindings for Uber's H3 library](https://github.com/uber/h3-js).
+#' This package uses package \href{https://github.com/jeroen/v8}{V8} to access the
+#' \href{https://github.com/uber/h3-js}{javascript bindings for Uber's H3 library}
 #'
 "_PACKAGE"
